c531bb95bcc1311cc36e1433b6f9f22259696747
brendasota <antonio.toche@utec.edu.pe>
2019-06-21 16:38:47 -0500

    Segunda actualización

---
+++ cmake-build-debug/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/cmake-build-debug/CMakeFiles/Objeto.cpp" "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/cmake-build-debug/CMakeFiles/game.dir/CMakeFiles/Objeto.cpp.o"
+  "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/cmake-build-debug/Controlador.cpp" "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/cmake-build-debug/CMakeFiles/game.dir/Controlador.cpp.o"
+  "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/Tierra.cpp" "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/cmake-build-debug/CMakeFiles/game.dir/Tierra.cpp.o"
+  "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/game.cpp" "/home/utec/Documentos/Pfinal/Proyecto-POO-C-/cmake-build-debug/CMakeFiles/game.dir/game.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
