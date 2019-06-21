4443000d4f5850b23afce8a40691f57462fd7a55
brendasota <40002586+brendasota@users.noreply.github.com>
2019-06-21 16:28:48 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,8 +2,28 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int matriz[3][3];
+    for(int x=0;x<3;x++){
+        for(int y=0;y<3;y++){
+            cout<<"Digita un numero ["<<x<<"]["<<y<<"]: ";
+            cin>>matriz[x][y];
+        }
+    }
+    cout<<"\nLa diagonal de los numeros ingresados es: "<<endl;
+    cout<<matriz[0][0]<<endl;
+    cout<<"  "<<matriz[1][1]<<endl;
+    cout<<"    "<<matriz[2][2]<<endl;
 
     return 0;
+
 }
 
 
