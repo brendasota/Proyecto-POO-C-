934e2723feeb824804846d75f61e959a9c59318e
brendasota <40002586+brendasota@users.noreply.github.com>
2019-06-25 15:01:10 -0500

    Update Controlador.h

--- Controlador.h
+++ Controlador.h
@@ -5,45 +5,35 @@
 #ifndef GAME_CONTROLADOR_H
 #define GAME_CONTROLADOR_H
 
+#include <limits>
 
 #include "Tierra.h"
 #include "Tipos.h"
 
+using namespace std;
 template <typename T>
 T input(string label) {
- T value;
- cout << label;
- cin >> value;
- cin.clear();
- cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
- return value;
+    T value;
+    cout << label;
+    cin >> value;
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    return value;
 }
 
-/*
-//--- Esta seria la funcion para leer un string,
-//--- esta función si se quiere generalizar para colocar un label
-//--- y leer cualquier tipo de dato se tendría que convertir al template anterior
-string&& input(string label) {
- string value;
- cout << label;
- cin >> value;
- return move(value);
-}
-*/
 class Controlador {
-TipoEntero opcion;
-   Tierra tierra;
-   void imprimirMenu();
-   void seleccionarOpcion();
-   void agregarObjeto();
-   void removerObjeto();
-   void dibujarMapa();
+    TipoEntero opcion;
+    Tierra tierra;
+    void imprimirControlador();
+    void seleccionarOpcion();
+    void agregarObjeto();
+    void removerObjeto();
+    void dibujarMapa();
 public:
-   Menu(): opcion{} {}
-   void ejecutar();
-};
-#endif
+    Controlador(): opcion{} {}
+    void ejecutar();
 };
 
 
 #endif //GAME_CONTROLADOR_H
+
