24eb24651a6b3e1878d9b8b5e30bc0ad3d98d59c
brendasota <40002586+brendasota@users.noreply.github.com>
2019-06-25 15:03:55 -0500

    Update Tierra.h

--- Tierra.h
+++ Tierra.h
@@ -1,10 +1,18 @@
-#ifndef AGREGACION_TIERRA_H
-#define AGREGACION_TIERRA_H
+//
+// Created by utec on 21/06/19.
+//
+
+#ifndef GAME_TIERRA_H
+#define GAME_TIERRA_H
+
 #include <iostream>
 #include <vector>
 #include "Tipos.h"
 #include "Objeto.h"
+#include <SFML/Graphics.hpp>
+
 using namespace std;
+
 // Valores constantes
 const TipoEntero ALTURA = 21;
 const TipoEntero ANCHO  = 21;
@@ -12,21 +20,22 @@ const TipoCaracter COLOR ='.';
 
 class Tierra {
 private:
-   vector<vector<char>> plano;
-   vector<Objeto*> objetos;
+    sf::RenderWindow* plano;
+    vector<Objeto*> objetos;
 public:
-   Tierra();
-   Tierra(TipoEntero altura, TipoEntero ancho);
-   virtual ~Tierra();
-   void adicionarObjeto(Objeto* objeto);
-   Objeto* removerObjeto(string& nombre);
-   void imprimirObjetos();
-   TipoEntero getAltura();
-   TipoEntero getAncho();
-   TipoEntero getCantidadObjectos();
-   void dibujarTierra();
-   void actualizarTierra();
+    Tierra();
+    Tierra(TipoEntero altura, TipoEntero ancho);
+    virtual ~Tierra();
+    void adicionarObjeto(Objeto* objeto);
+    Objeto* removerObjeto(string& nombre);
+    void imprimirObjetos();
+    TipoEntero getAltura();
+    TipoEntero getAncho();
+    TipoEntero getCantidadObjectos();
+    void dibujarTierra();
+    void actualizarTierra();
 };
 
-#endif //AGREGACION_TIERRA_H
+
+#endif
 
