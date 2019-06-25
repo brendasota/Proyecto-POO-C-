395f62ddc56e80df037a2dbfd3aee66d92862e05
brendasota <40002586+brendasota@users.noreply.github.com>
2019-06-24 22:26:18 -0500

    Update Controlador.h

--- cmake-build-debug/Controlador.h
+++ cmake-build-debug/Controlador.h
@@ -6,8 +6,44 @@
 #define GAME_CONTROLADOR_H
 
 
-class Controlador {
+#include "Tierra.h"
+#include "Tipos.h"
 
+template <typename T>
+T input(string label) {
+ T value;
+ cout << label;
+ cin >> value;
+ cin.clear();
+ cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+ return value;
+}
+
+/*
+//--- Esta seria la funcion para leer un string,
+//--- esta función si se quiere generalizar para colocar un label
+//--- y leer cualquier tipo de dato se tendría que convertir al template anterior
+
+string&& input(string label) {
+ string value;
+ cout << label;
+ cin >> value;
+ return move(value);
+}
+*/
+class Controlador {
+TipoEntero opcion;
+   Tierra tierra;
+   void imprimirMenu();
+   void seleccionarOpcion();
+   void agregarObjeto();
+   void removerObjeto();
+   void dibujarMapa();
+public:
+   Menu(): opcion{} {}
+   void ejecutar();
+};
+#endif
 };
 
 
