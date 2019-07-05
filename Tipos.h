f73621d55d21c9023907f63e99a942a60157c538
brendasota <40002586+brendasota@users.noreply.github.com>
2019-07-05 14:12:44 -0500

    Update Tipos.h

--- Tipos.h
+++ Tipos.h
@@ -1,12 +1,9 @@
-//
-// Created by utec on 21/06/19.
-//
-
-#ifndef GAME_TIPOS_H
-#define GAME_TIPOS_H
+#ifndef TIPOS
+#define TIPOS
 
 #include <string>
 using namespace std;
+
 // Definiendo alias
 using TipoEntero = int;
 using TipoCaracter = char;
