740f0b4d62ddeee84b1bb9e49c9e41a46903bc13
brendasota <40002586+brendasota@users.noreply.github.com>
2019-07-05 14:11:46 -0500

    Update Tierra.cpp

--- Tierra.cpp
+++ Tierra.cpp
@@ -1,96 +1,217 @@
-//
-// Created by utec on 21/06/19.
-//
 #include "Tierra.h"
 #include <string>
 #include <iomanip>
 #include <map>
+#include <iterator> 
 #include <algorithm>
 #include "Objeto.h"
-
-
-map<char, sf::Color> listaColores = {
-        {'R', sf::Color::Red },
-        {'G', sf::Color::Green},
-        {'B', sf::Color::Blue}};
-
+#include "Tierra.h"
 
 using namespace std;
-Tierra::Tierra() {
-    plano = nullptr;
+
+Tierra::Tierra() : ancho{}, altura{} {
+	plano = nullptr;
 }
-Tierra::Tierra(TipoEntero altura, TipoEntero ancho) {
-    plano = new sf::RenderWindow(sf::VideoMode(ancho, altura), "SFML works!");
+
+Tierra::Tierra(TipoEntero ancho, TipoEntero altura) : altura{ altura }, ancho{ ancho } {
+	plano = new sf::RenderWindow();
 }
+
 Tierra::~Tierra() {
-    delete plano;
+	delete plano;
 }
+
 void Tierra::adicionarObjeto(Objeto* objeto) {
-    objetos.emplace_back(objeto);
+	objetos.emplace_back(objeto);
 }
-Objeto* Tierra::removerObjeto(string& nombre) {
-// Si vector esta vacio
-    if (objetos.size() == 0)
-        return nullptr;
-// Buscando objeto
-    auto iter = find_if(begin(objetos), end(objetos),
-                        [&nombre](Objeto* obj){ return obj->getNombre() == nombre; });
-    if (iter != end(objetos)) {
-// Eliminando la referencia al puntero objeto dentro del vector objetos
-        objetos.erase(iter);
-//-- si encuentra al objeto lo separa del vector,
-//-- (no libera de memoria el objeto encontrado), esto se hará en el menú,
-//-- donde fue asignado, debido a que el objeto no es parte sino el objeto es un visitante.
-        return *iter;
-    }
-// Si vector esta vacio
-    return nullptr;
+
+bool Tierra::buscarObjeto(string nombre) {
+	// Buscando objeto
+	if (objetos.size() == 0)
+		return false;
+	string i_nombre;
+	for (auto & item : *vector) {
+		i_nombre = item->getNombre();
+		for (auto& c : i_nombre) c = toupper(c);
+		if (i_nombre == nombre) {
+			item->actualizarInformacion();
+			return true;
+		}
+	}
+	return false;
 }
+
 void Tierra::imprimirObjetos() {
-    int i = 0;
-    for (auto& item: objetos) {
-        cout << "* * * * * * [" << i << "] ";
-        cout << " Nombre = " << item->getNombre() << " "
-             << item->mostrarPosicion()
-             << " Color = " << item->getColor() << '\n';
-        i++;
-    }
+	int i = 0;
+	for (auto& item : *vector) {
+		cout << "  [" << i+1 << "]"<<endl;
+		item->mostrarInformacion();
+		cout << endl;
+		i++;
+	}
 }
+
 void Tierra::actualizarTierra() {
-//    for (auto &row: plano)
-//        for (auto &cell: row)
-//            cell = COLOR;
-//    for (auto& item: objetos)
-//        plano[item->getPosX()][item->getPosY()]
-//                = item->getColor();
+	plano->clear();
+
+	sf::Texture mapaT;
+	if (!mapaT.loadFromFile("img/map.png"))
+	{
+		cout << "Error al cargar el archivo\n";
+		system("pause");
+	}
+	mapaT.setRepeated(true);
+	sf::Sprite mapaS;
+	mapaS.setTexture(mapaT);
+	mapaS.setTextureRect(sf::IntRect(0, 0, 600, 400));
+	plano->draw(mapaS);
+
+	for (auto obj : *vector) {
+		sf::Texture puntoT;
+		if (!puntoT.loadFromFile(obj->getTexturefile()))
+		{
+			cout << "Error al cargar el archivo\n";
+			system("pause");
+		}
+		sf::Sprite puntoS;
+		puntoS.setTexture(puntoT);
+		puntoS.setPosition(static_cast<float>(obj->getPosX()), static_cast<float>(obj->getPosY()));
+		plano->draw(puntoS);
+	}
+	plano->display();
 }
+
 void Tierra::dibujarTierra() {
 
-    while (plano->isOpen())
-    {
-        sf::Event event;
-        while (plano->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                plano->close();
-        }
-        plano->clear();
-        for (auto obj: objetos) {
-            sf::CircleShape shape(10);
-            shape.setPosition(obj->getPosX(), obj->getPosY());
-            shape.setFillColor(listaColores[obj->getColor()]);
-            plano->draw(shape);
-        }
-        plano->display();
-    }
+	// Verifica si plano ha sido creado anteriormente
+	if (!plano->isOpen())
+		plano->create(sf::VideoMode(ancho, altura), "MAPA - Presione [ESC] para salir... ");
+	else
+		plano->display();
+
+	// Bucle principal
+	while (plano->isOpen()) {
+		capturarEventos();
+		actualizarTierra();
+	}
 }
+
+void Tierra::hallarMejores()
+{
+	multimap<int,Objeto*> mejores;
+	for (auto& item : objetos) {
+		mejores.insert(make_pair(item->getCalificacion(),item));
+	}
+	int i = 0;
+	for (auto itr = rbegin(mejores); itr != rend(mejores) && i < 3; ++itr, ++i) {
+		mejoresobjetos.emplace_back(itr->second);
+	}
+}
+
+void Tierra::hallarMejoresTipo()
+{
+	multimap<int, Objeto*> mejoresHotel;
+	multimap<int, Objeto*> mejoresMuseo;
+	multimap<int, Objeto*> mejoresRestaurante;
+	for (auto& item : objetos) {
+		Hotel* d = dynamic_cast<Hotel*>(item);
+		if (d) {
+			mejoresHotel.insert(make_pair(item->getCalificacion(), item));
+		}
+		else {
+			Museo* d = dynamic_cast<Museo*>(item);
+			if (d) {
+				mejoresMuseo.insert(make_pair(item->getCalificacion(), item));
+			}
+			else {
+				Restaurante* d = dynamic_cast<Restaurante*>(item);
+				if (d) {
+					mejoresRestaurante.insert(make_pair(item->getCalificacion(), item));
+				}
+			}
+		}
+	}
+	int i = 0;
+	if (!mejoresHotel.empty())
+		mejoresobjetostipo.emplace_back(rbegin(mejoresHotel)->second);
+	if (!mejoresMuseo.empty())
+		mejoresobjetostipo.emplace_back(rbegin(mejoresMuseo)->second);
+	if (!mejoresRestaurante.empty())
+		mejoresobjetostipo.emplace_back(rbegin(mejoresRestaurante)->second);
+}
+
+void Tierra::hallarDisponibles()
+{
+	for (auto& item : objetos) {
+		Hotel *d = dynamic_cast<Hotel*>(item);
+		if (d) {
+			if (toupper(d->getDisponibilidad()) == 'D')
+				objetosdisponibles.emplace_back(d);
+		}
+	}
+}
+
+void Tierra::hallarLujosos()
+{
+	multimap<int, Objeto*> lujosos;
+	for (auto& item : objetos) {
+		Hotel* d = dynamic_cast<Hotel*>(item);
+		if (d) {
+			lujosos.insert(make_pair(d->getEstrellas(), item));
+		}
+	}
+	int i = 0;
+	for (auto itr = rbegin(lujosos); itr != rend(lujosos) && i < 3; ++itr, ++i) {
+		objetoslujosos.emplace_back(itr->second);
+	}
+}
+
+void Tierra::actualizarVector(int operacion)
+{
+	switch (operacion) {
+	case 1:
+		vector = &objetos;
+		break;
+	case 2:
+		vector = &mejoresobjetos;
+		break;
+	case 3:
+		vector = &mejoresobjetostipo;
+		break;
+	case 4: 
+		vector = &objetosdisponibles;
+		break;
+	case 5:
+		vector = &objetoslujosos;
+		break;
+}
+}
+
 TipoEntero Tierra::getAltura() {
-    return plano->getSize().y;
+	return altura;
 }
-TipoEntero Tierra::getAncho(){
-    return plano->getSize().x;
+
+TipoEntero Tierra::getAncho() {
+	return ancho;
 }
 
 TipoEntero Tierra::getCantidadObjectos() {
-    return objetos.size();
+	return objetos.size();
+}
+
+void Tierra::capturarEventos() {
+	sf::Event event{};
+
+	while (plano->pollEvent(event)) {
+
+		switch (event.type) {
+		case sf::Event::Closed:
+			plano->close();
+			break;
+		case sf::Event::KeyPressed:
+			if (event.key.code == sf::Keyboard::Escape)
+				plano->close();
+			break;
+		}
+	}
 }
