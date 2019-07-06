ac7f2c824f259c74274fcde19f0099f392d531c1
AlejandriaA <51420143+AlejandriaA@users.noreply.github.com>
2019-07-06 16:11:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,33 +6,3 @@ int main() {
 	ctrl.ejecutar();
 	return 0;
 }
-
-//const map<char, sf::Color> listaColores = {
-//        {'R', sf::Color::Red },
-//        {'G', sf::Color::Green},
-//        {'B', sf::Color::Blue}};
-
-
-
-//int main()
-//{
-//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Green);
-//
-//    while (window.isOpen())
-//    {
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//        }
-//
-//        window.clear();
-//        window.draw(shape);
-//        window.display();
-//    }
-//
-//    return 0;
-//}
